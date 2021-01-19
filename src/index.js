acf9b0b6ee99d033ded21f163f41dd7c509950ef
Parfionov Anton <toystix1992@gmail.com>
2021-01-20 02:06:41 +0300

    feat: Add final solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (!array || array.length===0) return 0;
+    let sorted = array.slice().sort((a, b) => a - b)
+    let minVal = sorted[0];
+    return minVal;
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (!array || array.length===0) return 0;
+    let sorted = array.slice().sort((a, b) => a - b)
+    let maxVal = sorted[array.length-1];
+    return maxVal
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if(!array || array.length===0) return 0;
+    const sum= array.reduce((a, b) => a + b);
+    return sum/array.length;
 }
